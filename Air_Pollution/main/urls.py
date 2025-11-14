05747f1d75fafd9974f2203f563aff558f96b9e6
Aradrezvaninejad <agent.rt7@gmail.com>
2025-11-14 18:42:43 +0330

    added api completly

--- Air_Pollution/main/urls.py
+++ Air_Pollution/main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("",views.index , name="index"),
-
+    path("", views.index, name="index"),
+    path("air_history/", views.air_history, name="air_history"),
 ]
+
